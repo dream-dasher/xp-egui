285bd569e977043a84937c357c882f587d23f110
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-21 22:02:11 -0700

    Chore: clean up of test code bits

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ fn main() -> eframe::Result<()> {
             .with_min_inner_size([300.0, 220.0])
             .with_icon(
                 // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(&include_bytes!("../data/aus_gefang.png")[..])
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
                     .expect("Failed to load icon"),
             ),
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "xp-eGUI+eframe",
         native_options,
         Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
     )
