be02cb1623c99a54059c897ea28bd6afb0130600
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-30 22:57:18 -0400

    Chore: autoformatting, indent size change

--- examples/files.rs
+++ examples/files.rs
@@ -6,30 +6,30 @@ use walkdir::WalkDir;
 
 const DATA_DIR: &str = "data/example/";
 fn main() {
-    let path = Path::new(DATA_DIR);
+        let path = Path::new(DATA_DIR);
 
-    println!("----------------------------------------");
-    if path.is_dir() {
-        match fs::read_dir(path) {
-            Ok(entries) => {
-                for entry in entries {
-                    let entry = entry.unwrap();
-                    let path = entry.path();
-                    if path.is_file() {
-                        println!("File: {}", path.display());
-                    }
+        println!("----------------------------------------");
+        if path.is_dir() {
+                match fs::read_dir(path) {
+                        Ok(entries) => {
+                                for entry in entries {
+                                        let entry = entry.unwrap();
+                                        let path = entry.path();
+                                        if path.is_file() {
+                                                println!("File: {}", path.display());
+                                        }
+                                }
+                        }
+                        Err(e) => eprintln!("Failed to read directory: {}", e),
                 }
-            }
-            Err(e) => eprintln!("Failed to read directory: {}", e),
         }
-    }
-    println!("----------------------------------------");
-    for entry in WalkDir::new(path) {
-        let entry = entry.unwrap();
-        println!("{}", entry.path().display());
-    }
-    println!("----------------------------------------");
-    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-        println!("{}", entry.path().display());
-    }
+        println!("----------------------------------------");
+        for entry in WalkDir::new(path) {
+                let entry = entry.unwrap();
+                println!("{}", entry.path().display());
+        }
+        println!("----------------------------------------");
+        for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+                println!("{}", entry.path().display());
+        }
 }
--- src/boilerplate.rs
+++ src/boilerplate.rs
@@ -11,16 +11,17 @@ use tracing_subscriber::EnvFilter;
 /// - Not optimal.  But has more info than default and allows easily setting a default log level.
 /// - We should return a `Result`, but I don't want to mess with the flow of this repo's tour by adding different error types and consequent handling strategies.
 pub fn tracing_subscriber_boilerplate(log_default: &str) {
-    let filter =
-        EnvFilter::try_new(std::env::var("RUST_LOG").unwrap_or_else(|_| log_default.to_string()))
-            .expect("Valid filter input provided.");
+        let filter = EnvFilter::try_new(
+                std::env::var("RUST_LOG").unwrap_or_else(|_| log_default.to_string()),
+        )
+        .expect("Valid filter input provided.");
 
-    tracing_subscriber::fmt()
-        .pretty()
-        .with_env_filter(filter)
-        .with_file(true)
-        .with_line_number(true)
-        .with_thread_ids(true)
-        .with_target(true)
-        .init();
+        tracing_subscriber::fmt()
+                .pretty()
+                .with_env_filter(filter)
+                .with_file(true)
+                .with_line_number(true)
+                .with_thread_ids(true)
+                .with_target(true)
+                .init();
 }
