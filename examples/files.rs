4e52f7555ec56759788409bd92b8e112c8e11969
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-05-31 20:02:39 -0700

    Style

--- examples/files.rs
+++ examples/files.rs
@@ -1,35 +1,35 @@
 //! Very basic polars use.
 
-use std::fs;
-use std::path::Path;
+use std::{fs, path::Path};
+
 use walkdir::WalkDir;
 
 const DATA_DIR: &str = "data/example/";
 fn main() {
-        let path = Path::new(DATA_DIR);
+    let path = Path::new(DATA_DIR);
 
-        println!("----------------------------------------");
-        if path.is_dir() {
-                match fs::read_dir(path) {
-                        Ok(entries) => {
-                                for entry in entries {
-                                        let entry = entry.unwrap();
-                                        let path = entry.path();
-                                        if path.is_file() {
-                                                println!("File: {}", path.display());
-                                        }
-                                }
-                        }
-                        Err(e) => eprintln!("Failed to read directory: {}", e),
+    println!("----------------------------------------");
+    if path.is_dir() {
+        match fs::read_dir(path) {
+            Ok(entries) => {
+                for entry in entries {
+                    let entry = entry.unwrap();
+                    let path = entry.path();
+                    if path.is_file() {
+                        println!("File: {}", path.display());
+                    }
                 }
+            }
+            Err(e) => eprintln!("Failed to read directory: {}", e),
         }
-        println!("----------------------------------------");
-        for entry in WalkDir::new(path) {
-                let entry = entry.unwrap();
-                println!("{}", entry.path().display());
-        }
-        println!("----------------------------------------");
-        for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-                println!("{}", entry.path().display());
-        }
+    }
+    println!("----------------------------------------");
+    for entry in WalkDir::new(path) {
+        let entry = entry.unwrap();
+        println!("{}", entry.path().display());
+    }
+    println!("----------------------------------------");
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        println!("{}", entry.path().display());
+    }
 }
